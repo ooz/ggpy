b258dff4584dc46428b4f5468091cfd818874c17
ooz <ooz@users.noreply.github.com>
2020-05-21 01:19:17 +0200

    Test last_modified

--- test/test_gg.py
+++ test/test_gg.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import re
+
 import gg
 
 def test_is_root_readme():
     assert gg.is_root_readme('README.md')
+
+def test_last_modified():
+    assert re.match(r'''\d+-\d{2}-\d{2}''', gg.last_modified('README.md'))
