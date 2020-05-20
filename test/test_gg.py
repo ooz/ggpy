6ad3667cec8d94daf4ede7b2e389e34349005d94
ooz <ooz@users.noreply.github.com>
2020-05-21 01:38:41 +0200

    Test convert_canonical

--- test/test_gg.py
+++ test/test_gg.py
@@ -15,3 +15,8 @@ def test_convert_path():
     assert gg.convert_path('README.md') == 'index.html'
     assert gg.convert_path('test/README.md') == 'test/index.html'
     assert gg.convert_path('test/some-post.md') == 'test/some-post.html'
+
+def test_convert_canonical():
+    assert gg.convert_canonical('.', 'index.html') == 'https://ooz.github.io/ggpy/'
+    assert gg.convert_canonical('.', 'test/features/index.html') == 'https://ooz.github.io/ggpy/test/features/'
+    assert gg.convert_canonical('.', 'test/some-post.html') == 'https://ooz.github.io/ggpy/test/some-post.html'
