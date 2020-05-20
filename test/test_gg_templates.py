14d9e8ec1e95da37430a3f63a6672fb4cbe0b863
ooz <ooz@users.noreply.github.com>
2020-05-21 00:30:55 +0200

    Unittest sitemap generation

---
+++ test/test_gg_templates.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import gg
+
+def test_sitemap():
+    posts = [
+        {
+            'url': 'https://example.com/',
+            'last_modified': '2020-02-20'
+        },
+        {
+            'url': 'https://example.com/blog',
+            'last_modified': '2020-02-21'
+        }
+    ]
+    sitemap = gg.sitemap(posts)
+    assert sitemap == \
+'''<?xml version="1.0" encoding="utf-8" standalone="yes" ?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://example.com/</loc>
+    <lastmod>2020-02-20</lastmod>
+  </url>
+  <url>
+    <loc>https://example.com/blog</loc>
+    <lastmod>2020-02-21</lastmod>
+  </url>
+</urlset>
+'''
