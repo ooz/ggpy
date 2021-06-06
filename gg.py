3b580fe035a377218960f7a46082c01c95106372
ooz <ooz@users.noreply.github.com>
2021-06-06 03:42:48 +0200

    Partially revert 4ea32f7b2b5cb7f47cde60117e921fce42837213, since regression introduced back link on root README

--- gg.py
+++ gg.py
@@ -122,9 +122,9 @@ def post_header(title_html, date='', config=None):
 </div>'''
     return header
 
-def footer_navigation(root_url='', is_root=False):
+def footer_navigation(root_url='', is_index=False, is_root=False):
     nav = []
-    if len(root_url) and not is_root:
+    if len(root_url) and not is_index and not is_root:
         nav.append(f'''<a href="{root_url}" class="nav">back</a>''')
     nav.append('''<a href="#" class="nav">top</a>''')
     nav.append('''<a href="javascript:toggleTheme()" class="nav">🌓</a>''')
@@ -487,6 +487,9 @@ def convert_canonical(directory, targetpath, config=None):
             return f'{targetpath[:-10]}'
     return targetpath
 
+def is_root_readme(path):
+    return os.path.relpath(path) == 'README.md'
+
 def read_post(directory, filepath, config=None):
     markdown_content = read_file(filepath)
     post = markdown2post(markdown_content, config)
@@ -495,6 +498,7 @@ def read_post(directory, filepath, config=None):
     post['filepath'] = targetpath
     post['url'] = canonical_url
     post['last_modified'] = last_modified(filepath)
+    post['is_root'] = is_root_readme(filepath)
     post['is_index'] = TAG_INDEX in post['tags']
     post['html'] = template_page(post, config)
     return post
